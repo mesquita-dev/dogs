85fd6336feff49c288202e13c0b52a21b8e3d317
Lucas Mesquita <lmesquita2k@gmail.com>
2023-06-29 16:23:03 -0300

    Adding error message in form

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
+import useForm from "../../Hooks/useForm";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const username = useForm();
+  const password = useForm();
 
   function handleSubmit(event) {
     event.preventDefault();
 
+    if (username.validate() && password.validate()) {
     fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify(),
     })
       .then((response) => {
         return response.json();
@@ -23,6 +25,7 @@ const LoginForm = () => {
       .then((json) => {
         console.log(json);
       });
+    }
   }
 
   return (
@@ -30,8 +33,8 @@ const LoginForm = () => {
       <h1>Login</h1>
 
       <form action="" onSubmit={handleSubmit}>
-        <Input label="Usuário" type="text" name="username" />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
 
         <Button>Entrar</Button>
       </form>
